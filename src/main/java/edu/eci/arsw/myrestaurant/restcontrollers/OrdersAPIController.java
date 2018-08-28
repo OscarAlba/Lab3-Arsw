62ca710da21819bacc7a4607dfc44569f41f6340
Oscar Alba <oscaralba33@gmail.com>
2018-08-27 21:45:50 -0500

    first commit

---
+++ src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2016 Pivotal Software, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.eci.arsw.myrestaurant.restcontrollers;
+
+import com.google.gson.Gson;
+import edu.eci.arsw.myrestaurant.model.Order;
+import edu.eci.arsw.myrestaurant.model.ProductType;
+import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
+import edu.eci.arsw.myrestaurant.services.RestaurantOrderServices;
+import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class OrdersAPIController {
+
+ @Service   
+ @RestController
+ @RequestMapping(value = "/orders")
+ public class OrderController {
+  
+         @Autowired
+         RestaurantOrderServices restaurant;
+        
+ 	@RequestMapping(method = RequestMethod.GET)
+        public ResponseEntity<?> getOrders(){
+            System.out.println("______________POLO----------------");
+            //Gson gson = new Gson();
+            HashMap<String, Integer> map = new HashMap<>();
+            map.put("POS",3);
+            map.put("RES",9);
+            //String json = gson.toJson(map);
+            return new ResponseEntity<>(map,HttpStatus.OK);  
+ 	}      
+ }
+}
