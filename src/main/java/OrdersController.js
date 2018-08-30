87da3de9f75fe2c480edc9c590e72a8a150cdb2c
unknown <2106991@labinfo.is.escuelaing.edu.co>
2018-08-30 18:14:39 -0500

    First Commit

---
+++ src/main/java/OrdersController.js
@@ -0,0 +1,22 @@
+
+var orders =[];
+var order1= {
+    "order_id": 1,
+	"table_id": 1,
+	"products": [{
+			"product": "PIZZA",
+			"quantity": 3,
+			"price": "$15.000"
+		},
+		{
+			"product": "HAMBURGER",
+			"quantity": 1,
+			"price": "$12.300"
+		}
+	]
+
+};
+
+function addTable(){
+    
+}
